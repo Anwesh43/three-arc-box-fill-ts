917a18e06347d3907bdc6b4c95bf9c79349275e1
Anwesh43 <anweshthecool0@gmail.com>
2020-10-18 18:27:07 +0530

    adding Renderer class

--- index.ts
+++ index.ts
@@ -177,3 +177,48 @@ class TABFNode {
         return this 
     }
 }
+
+class ThreeArcBoxFill {
+    
+    curr : TABFNode = new TABFNode(0)
+    dir : number = 1 
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)    
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
+
+class Renderer {
+
+    tabf : ThreeArcBoxFill = new ThreeArcBoxFill()
+    animator : Animator = new Animator() 
+
+    render(context : CanvasRenderingContext2D) {
+        this.tabf.draw(context)
+    }
+
+    handleTap(cb : Function) {
+        this.tabf.startUpdating(() => {
+            this.animator.start(() => {
+                cb()
+                this.tabf.update(() => {
+                    this.animator.stop()
+                    cb()
+                })
+            })
+        })
+    }
+}
