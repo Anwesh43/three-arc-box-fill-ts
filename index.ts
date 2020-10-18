f4e5bc8f54abbff0ff23d90e0e4b949f0b8a4622
Anwesh43 <anweshthecool0@gmail.com>
2020-10-18 18:04:58 +0530

    adding Stage class

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import { timeStamp } from "console"
+
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 const parts : number = 3 
@@ -55,5 +57,36 @@ class DrawingUtil {
     static drawTABFNode(context : CanvasRenderingContext2D, i : number, scale : number) {
         context.fillStyle = colors[i]
         DrawingUtil.drawThreeArcBoxFill(context, scale)
+    }
+}
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D 
+
+    initCanvas() {
+        this.canvas.width = w 
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+    
+    render() {
+        this.context.fillStyle = backColor 
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
     }
 }
