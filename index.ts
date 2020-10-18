e8475e649a60ec2942a09dda5369032a8fffb0fe
Anwesh43 <anweshthecool0@gmail.com>
2020-10-18 18:12:00 +0530

    adding State class

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { timeStamp } from "console"
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 const parts : number = 3 
+const scGap : number = 0.02 / parts 
 const sizeFactor : number = 3.9 
 const delay : number = 20 
 const rot : number = Math.PI / 2 
@@ -88,5 +89,29 @@ class Stage {
         stage.initCanvas()
         stage.render()
         stage.handleTap()
+    }
+}
+
+class State {
+
+    scale : number = 0 
+    dir : number = 0 
+    prevScale : number = 0 
+
+    update(cb : Function) {
+        this.scale += this.dir * scGap 
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir 
+            this.dir = 1 
+            this.prevScale = this.scale 
+            cb()
+        }
+    }
+
+    startUpdating(cb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale 
+            cb()
+        }
     }
 }
